a5a0018681f6e28e5e3e05a6714e865c8315f9bc
Timo <tj.timo.j@gmail.com>
2018-02-23 12:04:04 +0100

    versioning

--- app/code/Wuunder/Wuunderconnector/Controller/adminhtml/Index/Label.php
+++ app/code/Wuunder/Wuunderconnector/Controller/adminhtml/Index/Label.php
@@ -215,7 +215,7 @@ class Label extends \Magento\Framework\App\Action\Action
             'delivery_address' => $customerAdr,
             'pickup_address' => $webshopAdr,
             'preferred_service_level' => $preferredServiceLevel,
-            'source' => array("product" => "Magento 2 extension", "version" => array("build" => "1.0.1", "plugin" => "1.0"))
+            'source' => array("product" => "Magento 2 extension", "version" => array("build" => "1.0.2", "plugin" => "1.0"))
         );
     }
 
