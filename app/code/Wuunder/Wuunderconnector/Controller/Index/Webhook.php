2eaad236f4ee0dfd913874570a9610c02e13684c
kFrolichs <k.frolichs@gmail.com>
2018-03-30 12:39:02 +0200

    Added shipping update, works apart from saveTransaction.

--- app/code/Wuunder/Wuunderconnector/Controller/Index/Webhook.php
+++ app/code/Wuunder/Wuunderconnector/Controller/Index/Webhook.php
@@ -12,10 +12,11 @@ class Webhook extends \Magento\Framework\App\Action\Action
 
     protected $scopeConfig;
 
-    public function __construct(Context $context, \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, Data $helper)
+    public function __construct(Context $context, \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, Data $helper, \Magento\Framework\DB\Transaction $transactionFactory)
     {
-        $this->helper = $helper;
         $this->scopeConfig = $scopeConfig;
+        $this->helper = $helper;
+        $this->transactionFactory = $transactionFactory;
         parent::__construct($context);
     }
 
@@ -36,19 +37,70 @@ class Webhook extends \Magento\Framework\App\Action\Action
                 $sql = "UPDATE " . $tableName . " SET label_id = '".$result['id']."', label_url = '".$result['label_url']."', tt_url = '".$result['track_and_trace_url']."' WHERE order_id = ".$this->getRequest()->getParam('order_id');
     //            $connection->query($sql, array($result['id'], $result['label_url'], $result['track_and_trace_url'], $this->getRequest()->getParam('order_id')));
                 $connection->query($sql);
-                $this->ship($this->getRequest()->getParam('order_id'));
             } else if ($result['action'] === "track_and_trace_updated"){
                 $this->helper->log("Webhook - Track and trace for order: " . $this->getRequest()->getParam('order_id'));
+                $this->ship($this->getRequest()->getParam('order_id'), $result['carrier_code'], $result['track_and_trace_code']);
             }
         }
 
     }
 
-    private function ship($order_id) {
+    private function ship($order_id, $carrier, $label_id) {
+
+        $email = true;
+        $includeComment = false;
+        $comment = "Order Completed And Shipped";
+
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $order = $objectManager->create('\Magento\Sales\Model\Order') ->load($order_id);
-        $orderState = $this->scopeConfig->getValue('wuunder_wuunderconnector/advanced/post_booking_status');
-        $order->setState($orderState)->setStatus($this->scopeConfig->getValue('wuunder_wuunderconnector/advanced/post_booking_status'));
-        $order->save();
+
+        if ($order->canShip()) {
+            $this->helper->log("Can ship");
+            $convertOrder = $objectManager->create('\Magento\Sales\Model\Convert\Order');
+            $shipment = $convertOrder->toShipment($order);
+            foreach ($order->getAllItems() as $orderItem) {
+              if (!$orderItem->getQtyToShip() || $orderItem->getIsVirtual()) {
+                  continue;
+              }
+              $qtyShipped = $orderItem->getQtyToShip();
+              $shipmentItem = $convertOrder->itemToShipmentItem($orderItem)->setQty($qtyShipped);
+              $shipment->addItem($shipmentItem);
+            }
+$this->helper->log("Adding track and trace");
+            $track = $objectManager->create('\Magento\Sales\Model\Order\Shipment\TrackFactory')->create();
+            $track->setNumber($label_id);
+            $track->setCarrierCode('wuunder');
+            $track->setTitle($carrier);
+            $shipment->addTrack($track);
+
+$this->helper->log("Registering Shipment");
+            $shipment->register();
+            $shipment->addComment($comment);
+            $shipment->setEmailSent(true);
+            $shipment->getOrder()->setIsInProcess(true);
+$this->helper->log("Shipment set: in process");
+
+            try {
+                $saveTransaction = $this->transactionFactory->create();
+                $saveTransaction->addObject($shipment)
+                                ->addObject($shipment->getOrder())
+                                ->save();
+                $this->helper->log("transaction saved");
+            } catch (\Exception $e) {
+                $this->helper->log($e->getMessage());
+            }
+
+            // $shipment->sendEmail($email, ($includeComment ? $comment : ''));
+
+            $orderState = $this->scopeConfig->getValue('wuunder_wuunderconnector/advanced/post_booking_status');
+            $order->setState($orderState)->setStatus($this->scopeConfig->getValue('wuunder_wuunderconnector/advanced/post_booking_status'));
+            $order->save();
+$this->helper->log("Order status updated");
+
+            $shipment->save();
+            $this->helper->log("Shipment saved");
+        } else {
+            $this->helper->log("Error: Cannot ship");
+        }
     }
 }
