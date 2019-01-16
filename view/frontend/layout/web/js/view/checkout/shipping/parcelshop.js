d620d915cdffb98c78c7d31a7c1172ea1f970e06
Tim Dohmen <tim.dohmen@wearewuunder.com>
2019-01-16 19:51:57 +0100

    WIP add block for parcelship picker

---
+++ view/frontend/layout/web/js/view/checkout/shipping/parcelshop.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent'
+
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Wuunder_Wuunderconnector/checkout/shipping/additional-block'
+        }
+    });
+});
