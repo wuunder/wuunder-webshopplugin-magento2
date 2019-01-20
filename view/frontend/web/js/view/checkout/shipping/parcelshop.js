83279e35e99b58539ac14a8e5eab7024d2ea18b5
Tim Dohmen <tim.dohmen@wearewuunder.com>
2019-01-20 18:58:24 +0100

    Removed unneeded files and code

--- view/frontend/web/js/view/checkout/shipping/parcelshop.js
+++ view/frontend/web/js/view/checkout/shipping/parcelshop.js
@@ -3,19 +3,15 @@ define([
     'ko',
     'uiComponent',
     'Magento_Checkout/js/model/quote',
-    'Magento_Checkout/js/model/shipping-service',
-    'Magento_Checkout/js/checkout-data',
-    'Magento_Checkout/js/model/step-navigator',
-    'Magento_Checkout/js/model/sidebar',
-    'Magento_Checkout/js/view/shipping-information/address-renderer/default'
-], function ($, ko, Component, quote, shippingService, checkoutData, stepNavigator, sidebarMode) {
+
+], function ($, ko, Component, quote) {
     'use strict';
 
     return Component.extend({
-        defaults: {
-            template: 'Wuunder_Wuunderconnector/checkout/shipping/parcelshop',
-            isVisible: false
-        },
+        // defaults: {
+        //     template: 'Wuunder_Wuunderconnector/checkout/shipping/parcelshop',
+        //     isVisible: false
+        // },
 
         initObservable: function () {
 
@@ -31,16 +27,18 @@ define([
 
             var parcelShops = null;
 
+            var parcelshopShippingMethodElem;
+
             this.selectedMethod = ko.computed(function () {
-                var method = quote.shippingMethod();
-                var selectedMethod = method !== null ? method.carrier_code + '_' + method.method_code : null;
+                var parcelshopShippingMethodElem = quote.shippingMethod();
+                var selectedMethod = parcelshopShippingMethodElem !== null ? parcelshopShippingMethodElem.carrier_code + '_' + parcelshopShippingMethodElem.method_code : null;
 
                 if (selectedMethod === 'parcelshop-picker_parcelshop-picker') {
                     if ($('#wuunder_parcelshop_container').length === 0) {
                         var columnCount = $('#label_method_parcelshop-picker_parcelshop-picker').parent().children().length;
 
                         $('<tr><td id="wuunder_parcelshop_container" colspan="' + columnCount + '"></td><tr>').insertAfter($('#label_method_parcelshop-picker_parcelshop-picker').parent());
-                        $('#wuunder_parcelshop_container').html('<div id="parcelshop" class="parcelshopwrapper"><a href="#" id="get_parcels_link" >' + $.mage.__('Click here to select your Parcelshop') + '</a><div id="map_container"><div id="map_canvas" class="gmaps"></div></div></div>');
+                        $('#wuunder_parcelshop_container').html('<div id="parcelshop" class="parcelshopwrapper"><a href="#" id="get_parcels_link" onclick="_showParcelshopLocator()">' + $.mage.__('Click here to select your Parcelshop') + '</a><div id="map_container"><div id="map_canvas" class="gmaps"></div></div></div>');
                     }
                 } else {
                     $('#wuunder_parcelshop_container').remove();
@@ -50,151 +48,159 @@ define([
             }, this);
 
             $(document).ready(function () {
-
-                $(document).on('click', '.parcelshoplink', function (e) {
-                    e.preventDefault();
-                    var shopId = e.target.id;
-                    if (!shopId) {
-                        shopId = e.target.parentNode.id;
-                    }
-
-                    var parcelShop = parcelShops[shopId];
-                    var shippingAddressData = checkoutData.getShippingAddressFromData();
-
-                    window.wuunderShippingAddress = parcelShop;
+                // var shippingCarrierId = ""; //hardcoden
+                // // Get the modal
+                // var shippingMethodElems = jQuery('input.delivery_option_radio');
+                // var shippingAddress;
+                // //var parcelshopAddress = _markupParcelshopAddress("");
+                // var baseUrl;
+                // var baseUrlApi;
+                // var availableCarrierList;
+                // var getAddressUrl = "index.php?fc=module&module=wuunderconnector&controller=parcelshop&getAddress=1";
+                // var setParcelshopId = "index.php?fc=module&module=wuunderconnector&controller=parcelshop&setParcelshopId=1";
+                // var addressId = "";
+                // function initParcelshopLocator(url, apiUrl, carrierList) {
+                //     console.log(parcelshopShippingMethodElem);
+                //     baseUrl = url;
+                //     baseUrlApi = apiUrl;
+                //     availableCarrierList = carrierList;
                     
-                    var newShippingAddress = {
-                        firstName:"Parcelshop: ",
-                        lastName: parcelShop.company,
-                        street: {0:parcelShop.houseno, 1:""},
-                        postcode: parcelShop.zipcode,
-                        city: parcelShop.city,
-                        country_id: parcelShop.country,
-                        company: "",
-                        region: "",
-                        region_id: null,
-                        telephone: ""
-                    };
+                //     jQuery('.delivery_options').append('<div class="delivery_option alternate_item parcelshop_container"></div>');
 
-                    var shippingAddressData = checkoutData.getShippingAddressFromData();
+                //     if (parcelshopShippingMethodElem) {
+                //         //parcelshopShippingMethodElem.onchange = _onShippingMethodChange;
+                //         if (parcelshopAddress !== "") {
+                //             parcelshopId = "";
+                //         }
+                //         //jQuery(shippingMethodElems).change(_onShippingMethodChange);
+                //         jQuery(shippingMethodElems).on('change', _onShippingMethodChange);
+                //         _onShippingMethodChange();
+                //     }
+                // }
 
-                    var parcelShopCountry = '';
-                    parcelShopCountry = parcelShop.country;
+                // function _onShippingMethodChange() {
+                //     if (parcelshopShippingMethodElem.checked) {      
+                //         var container = document.createElement('div');
+                //         container.className += "chooseParcelshop";
+                //         container.innerHTML = '<div id="parcelshopsSelectedContainer"><a href="#/" onclick="_showParcelshopLocator()" id="selectParcelshop">Klik hier om een parcelshop te kiezen</a></div>';
+                //         // window.parent.document.getElementsByClassName('shipping')[0].appendChild(container);
+                //         window.parent.document.getElementsByClassName('parcelshop_container')[0].appendChild(container);
+                //         _printParcelshopAddress();
+                //     } else {
+                //         var containerElems = window.parent.document.getElementsByClassName('chooseParcelshop');
+                //         if (containerElems.length) {
+                //             containerElems[0].remove();
+                //         }
+                //     }
+                // }
 
-                    jQuery('.wuunder-shipping-information').show();
-                    jQuery('#wuunder_company').html('Wuunder Parcleshop: ' + parcelShop.company);
-                    jQuery('#wuunder_street').html(parcelShop.houseno);
-                    jQuery('#wuunder_zipcode_and_city').html(parcelShop.zipcode + ' ' + parcelShop.city);
-                    jQuery('#wuunder_country').html(parcelShopCountry);
+                // // add selected parcelshop to page
+                // function _printParcelshopAddress() {
+                //     if (parcelshopAddress) {
+                //         if (window.parent.document.getElementsByClassName("parcelshopInfo").length) {
+                //             window.parent.document.getElementsByClassName("parcelshopInfo")[0].remove();
+                //         }
+                //         var currentParcelshop = document.createElement('div');
+                //         currentParcelshop.className += 'parcelshopInfo';
+                //         currentParcelshop.innerHTML = '<br/><strong>Huidige Parcelshop:</strong><br/>' + parcelshopAddress;
+                //         window.parent.document.getElementById('parcelshopsSelectedContainer').appendChild(currentParcelshop);
+                //         window.parent.document.getElementById('selectParcelshop').innerHTML = 'klik hier om een andere parcelshop te kiezen';
 
-                    jQuery.cookie('wuunder-selected-parcelshop-company', 'wuunder Parcleshop: ' + parcelShop.company);
-                    jQuery.cookie('wuunder-selected-parcelshop-street', parcelShop.houseno);
-                    jQuery.cookie('wuunder-selected-parcelshop-zipcode', parcelShop.zipcode + ' ' + parcelShop.city);
-                    jQuery.cookie('wuunder-selected-parcelshop-country', parcelShopCountry);
-
-                    jQuery.ajax({
-                        method: 'POST',
-                        showLoader: true, // enable loader
-                        url : window.checkoutConfig.wuunder_parcelshop_save_url,
-                        data : parcelShops[shopId]
-                    }).done(function (response) {
-                        $('#map_canvas').empty();
-                        $('#map_canvas').html(response);
-                    });
-                });
-
-                $(document).on('click', '.invalidateParcel', function (e) {
-                    getParcels(e); });
-                $(document).on('click', '#get_parcels_link', function (e) {
-                    getParcels(e); });
+                //     }
+                // }
 
 
-                function getParcels(e)
-                {
-                    e.preventDefault();
+                // function _showParcelshopLocator() {
+                //     var address = "";
 
-                    var shippingAddress = quote.shippingAddress();
+                //     //jQuery.post( baseUrl + getAddressUrl + "&addressId=" + addressId, function( data ) {
+                //         //shippingAddress = data["address1"] + ' ' + data["postcode"] + ' ' + data["city"] + ' ' + data["country"];
+                //         shippingAddress = "Noorderpoort 69 5916PJ Venlo Nederland";
+                //         _openIframe();
+                //     //});
+                // }
 
-                    $('#get_parcels_link').hide();
 
-                    jQuery.ajax({
-                        method: 'POST',
-                        showLoader: true, // enable loader
-                        url : window.checkoutConfig.wuunder_parcelshop_url,
-                        data : {
-                            postcode: shippingAddress.postcode,
-                            countryId: shippingAddress.countryId,
-                            street: shippingAddress.street
-                        }
-                    }).done(function (response) {
-                        var map_canvas = $('#map_canvas');
+                // function _openIframe() {
+                //     var iframeUrl = baseUrlApi + 'parcelshop_locator/iframe/?lang=nl&availableCarriers=' + availableCarrierList + '&address=' + encodeURI(shippingAddress);
 
-                        if (response.success) {
-                            map_canvas.height(window.checkoutConfig.wuunder_googlemaps_height);
-                            map_canvas.width(window.checkoutConfig.wuunder_googlemaps_width);
+                //     var iframeContainer = document.createElement('div');
+                //     iframeContainer.className = "parcelshopPickerIframeContainer";
+                //     iframeContainer.onclick = function() { removeElement(iframeContainer); };
+                //     var iframeDiv = document.createElement('div');
+                //     iframeDiv.innerHTML = '<iframe src="' + iframeUrl + '" width="100%" height="100%">';
+                //     iframeDiv.className = "parcelshopPickerIframe";
+                //     iframeDiv.style.cssText = 'position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 2147483647';
+                //     iframeContainer.appendChild(iframeDiv);
+                //     window.parent.document.getElementsByClassName("chooseParcelshop")[0].appendChild(iframeContainer);
 
-                            var map = new google.maps.Map(map_canvas.get(0), {
-                                mapTypeId: google.maps.MapTypeId.ROAsetParcelshopImageDMAP
-                            });
+                //     function removeServicePointPicker() {
+                //         removeElement(iframeContainer);
+                //     }
 
-                            var markerBounds = new google.maps.LatLngBounds();
+                //     function onServicePointSelected(messageData) {
+                //         removeServicePointPicker();
+                //         _loadSelectedParcelshopAddress(messageData.parcelshopId);
+                //     }
 
-                            var marker_image = new google.maps.MarkerImage(response.gmapsIcon, new google.maps.Size(57, 62), new google.maps.Point(0, 0), new google.maps.Point(0, 31));
-                            var shadow = new google.maps.MarkerImage(response.gmapsIconShadow, new google.maps.Size(85, 55), new google.maps.Point(0, 0), new google.maps.Point(0, 55));
-                            var infowindow = new google.maps.InfoWindow();
-                            window.markers = new Array();
+                //     function onServicePointClose() {
+                //         removeServicePointPicker();
+                //     }
 
-                            parcelShops = response.parcelshops;
+                //     function onWindowMessage(event) {
+                //         var origin = event.origin,
+                //             messageData = event.data;
+                //         var messageHandlers = {
+                //             'servicePointPickerSelected': onServicePointSelected,
+                //             'servicePointPickerClose': onServicePointClose
+                //         };
+                //         if (!(messageData.type in messageHandlers)) {
+                //             alert('Invalid event type');
+                //             return;
+                //         }
+                //         var messageFn = messageHandlers[messageData.type];
+                //         messageFn(messageData);
+                //     }
 
-                            $.each(response.parcelshops, function (index, shop) {
+                //     window.addEventListener('message', onWindowMessage, false);
+                // }
 
-                                var content = shop.gmapsMarkerContent;
+                // function _loadSelectedParcelshopAddress(id) {
+                //         jQuery.post( baseUrl + setParcelshopId, {
+                //                 'parcelshopId' : id,
+                //         }, function( data ) {
+                //                 parcelshopAddress = _markupParcelshopAddress(data);
+                //                 _printParcelshopAddress();
+                //     });
+                // }
 
-                                var marker = new google.maps.Marker({
-                                    map: map,
-                                    position: new google.maps.LatLng(shop.gmapsCenterlat, shop.gmapsCenterlng),
-                                    icon: marker_image,
-                                    shadow: shadow
-                                });
+                // function _markupParcelshopAddress(parcelshopData) {
+                //                 data = JSON.parse(parcelshopData);
+                //                 var parcelshopInfoHtml = _capFirst(data.company_name) + "<br>" + _capFirst(data.address.street_name) +
+                //                 " " + data.address.house_number + "<br>" + data.address.city;
+                //                 parcelshopInfoHtml = parcelshopInfoHtml.replace(/"/g, '\\"').replace(/'/g, "\\'");
+                //                 return parcelshopInfoHtml;
+                // }
 
-                                markerBounds.extend(new google.maps.LatLng(shop.gmapsCenterlat, shop.gmapsCenterlng));
+                // // Capitalizes first letter of every new word.
+                // function _capFirst(str) {
+                //     if (str === undefined)
+                //         return "";
+                //     return str.replace(/\w\S*/g, function (txt) {
+                //         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                //     });
+                // }
 
-                                window.markers.push(marker);
-                                google.maps.event.addListener(marker, 'click', (function (marker) {
-                                    return function () {
-                                        infowindow.setContent(content);
-                                        infowindow.open(map, marker);
-                                    }
-                                })(marker));
+                // function removeElement(element) {
+                //     if (element.remove !== undefined) {
+                //         element.remove();
+                //     } else {
+                //         element && element.parentNode && element.parentNode.removeChild(element);
+                //     }
+                    
+                // }
 
-                                map.fitBounds(markerBounds);
-                            });
-                        } else {
-                            $('#get_parcels_link').show();
-                            // Set the error message
-                            map_canvas.html(response.error_message);
-                        }
-                    });
-
-                };
             });
-
-
-            return this;
-        },
-
-        initializeWuunderShopInfo: function () {
-            if (this.selectedMethod() == 'parcelshop-picker_parcelshop-picker') {
-                var WuunderCompany = jQuery.cookie('wuunder-selected-parcelshop-company');
-
-                if (typeof WuunderCompany !== null) {
-                    jQuery('.wuunder-shipping-information').show();
-                    jQuery('#wuunder_company').html(wuunderCompany);
-                    jQuery('#wuunder_street').html(jQuery.cookie('wuunder-selected-parcelshop-street'));
-                    jQuery('#wuunder_zipcode_and_city').html(jQuery.cookie('wuunder-selected-parcelshop-zipcode'));
-                    jQuery('#wuunder_country').html(jQuery.cookie('wuunder-selected-parcelshop-country'));
-                }
-            }
         },
 
 
