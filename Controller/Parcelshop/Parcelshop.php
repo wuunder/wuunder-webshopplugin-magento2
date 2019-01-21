113b2c64f5e15b3b1deb6479dbde4017d2369116
Tim Dohmen <thj.dohmen@gmail.com>
2019-01-21 12:07:23 +0100

    Added controller

---
+++ Controller/Parcelshop/Parcelshop.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Magento\CustomControllers\Controller\Demonstration;
+
+class Parcelshop extends \Magento\Framework\App\Action\Action
+{
+/**
+* say hello text
+*/
+public function execute()
+{
+die("Hello - Magento\\CustomControllers\\Controller\\Demonstration\\Sayhello - execute() method");
+}
+}
