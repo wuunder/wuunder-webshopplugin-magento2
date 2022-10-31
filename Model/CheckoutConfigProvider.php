148595547eef451bbaf71288284e764ff4998357
Maarten van Beek <37626165+scripting4u@users.noreply.github.com>
2022-10-31 14:57:31 +0100

    add is null

--- Model/CheckoutConfigProvider.php
+++ Model/CheckoutConfigProvider.php
@@ -37,7 +37,7 @@ class CheckoutConfigProvider implements \Magento\Checkout\Model\ConfigProviderIn
         $baseApiUrl = substr($tmpEnvironment->getStageBaseUrl(), 0, -3);
         $output['api_base_url'] = $baseApiUrl;
         $output['backend_base_url'] = $this->storeManager->getStore()->getBaseUrl();
-        if(empty($this->scopeConfig->getValue('carriers/parcelshopPicker/enabledcarriers'))){
+        if(empty($this->scopeConfig->getValue('carriers/parcelshopPicker/enabledcarriers')) || $this->scopeConfig->getValue('carriers/parcelshopPicker/enabledcarriers') == NULL){
             $output['available_carriers'] = null;
         } else {
             $output['available_carriers'] = str_replace(
